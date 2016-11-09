b6da692213c7bb5b04f7b2a44c612a69897f8f7b
Gluneko <844410616@qq.com>
2016-11-09 11:31:31 +0800

    Add nytimes

--- js/script.js
+++ js/script.js
@@ -11,13 +11,33 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
     // load streetview
-    var streetStr=$('#street').val();
-    var cityStr=$('#city').val();
-    var address=streetStr+', '+cityStr;
-    $greeting.text('So, you want to live at '+address+'?');
-    var streetViewUrl='http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
-    $body.append('<img class="bgimg" src="'+streetViewUrl+'">');
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    var nytimesUrl='https://api.nytiimes.com/svc/search/v2/articlesearch.json?q='
+    +cityStr+'&sort=newest&api-key=d3bd157e30f348569a18c42d61ba39fa';
+    $.getJSON(nytimesUrl,function(data){
+        $nytHeaderElem.text('New York Times Articles About '+cityStr);
+        var articles=data.response.docs;
+        articles.forEach( function(article) {
+            $nytElem.append('<li class="article">'+
+            '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+            '<p>'+article.snippet+'</p>'+'</li>');
+        });
+    }).fail(function () {
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
     return false;
 };
 
