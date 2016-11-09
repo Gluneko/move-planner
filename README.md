ea5bccbd54fa95d590495ab9e7e202975d30c625
Gluneko <844410616@qq.com>
2016-11-10 00:06:17 +0800

    Add wikipedia and README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # move-planner
+
+### Get started
+
+- Download the project from Github and open index.html.
+- You need an active internet connection since project uses several APIs and jQuery.
+- Input street and city information where you want to move to.
+- Click submit button to see the street view in the background, relevant wikipedia and NY Times articles about this city!
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -24,18 +23,39 @@ function loadData() {
 
 
     // load nytimes
-    var nytimesUrl='https://api.nytiimes.com/svc/search/v2/articlesearch.json?q='
-    +cityStr+'&sort=newest&api-key=d3bd157e30f348569a18c42d61ba39fa';
-    $.getJSON(nytimesUrl,function(data){
-        $nytHeaderElem.text('New York Times Articles About '+cityStr);
-        var articles=data.response.docs;
-        articles.forEach( function(article) {
-            $nytElem.append('<li class="article">'+
-            '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-            '<p>'+article.snippet+'</p>'+'</li>');
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+        cityStr + '&sort=newest&api-key=d3bd157e30f348569a18c42d61ba39fa';
+    $.getJSON(nytimesUrl, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        var articles = data.response.docs;
+        articles.forEach(function(article) {
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' + '</li>');
         });
-    }).fail(function () {
+    }).fail(function() {
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    // load Wikipedia
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +
+        cityStr + '&format=json&callback=wikiCallback';
+    // since fail function doesn't work on jsonp
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text('failed to get wikipedia resources');
+    }, 8000);
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+    }).done(function(response) {
+        var articleList = response[1];
+        articleList.forEach(function(articleStr) {
+            var url = 'https://en.wikipedia.org/wiki/' + articleStr;
+            $wikiElem.append('<li><a href="' + url + '">' + articleStr +
+                '</a></li>');
+        });
+        // if succeed,stop the timeout from happening
+        clearTimeout(wikiRequestTimeout);
     });
 
     return false;
